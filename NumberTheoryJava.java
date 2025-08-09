f1170654b8ff9b7fe513c37c67fc8cad6ad77c7e
ianwa09 <ianwa09+git@gmail.com>
2025-08-09 19:11:02 +0800

    iteration 3
    added a way to keep track of the progress made for each calculation.
    will show estimated time to completion and total fraction of tasks completed

--- NumberTheoryJava.java
+++ NumberTheoryJava.java
@@ -55,39 +55,56 @@ public class NumberTheoryJava {
         // n=1 -> 2
         if (limit >= 2)
             count++;
+        long x = 0;
 
         long nMax = (long) Math.floor(Math.sqrt(limit - 1));
+
+        long totalEven = (nMax - 2) / 2 + 1;
+        long m = (totalEven * 3) / 5;
+        long sec = m / (20000);
         for (long n = 2; n <= nMax; n += 2) {
             int r5 = (int) (n % 5);
             if ((r5 == 2 || r5 == 3) && n != 2)
                 continue;
+
+            x++;
+            sec = (m - x) / 20000;
+
             long value = n * n + 1;
             if (isProbablePrime(value))
                 count++;
+            System.out.print(
+                    "\r" + x + "/" + m + " tasks completed. (" + (100 * x / m) + "%) | Est: " + sec + " sec left. (~"
+                            + sec / 60 + " minutes (~" + sec / 3660 + " hours)).");
+
         }
         return count;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        long x = (long) Math.pow(10, 16);
+        long order = 1;
         while (true) {
             try {
-                System.out.print("Enter the limit x (0 to quit): ");
-                long x = scanner.nextLong();
-                if (x == 0)
-                    break;
+                // System.out.print("Enter the limit x (0 to quit): ");
+                // long x = scanner.nextLong();
+                // if (x == 0)
+                // break;
+
+                x = x * 10;
 
                 long count = countPrimes(x);
                 // List<Long> result = findPrimes(x); // Uncomment to also build the list
-                System.out.println("Count: " + count);
+                System.out.println("Order: " + order + " | Count: " + count);
                 System.out.println();
+                order++;
             } catch (Exception e) {
                 System.out.println("Enter a valid integer.");
                 scanner.nextLine(); // clear invalid input
             }
         }
 
-        scanner.close();
+        // scanner.close();
     }
 }
